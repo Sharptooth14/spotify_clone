579495458e0e0a790e26ca003dd6d366385288ae
Sharptooth14 <shreyashcr9@gmail.com>
2023-03-03 16:00:54 +0530

    topPlayCard completed

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -11,11 +11,31 @@ import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 import 'swiper/css';
 import 'swiper/css/free-mode';
 
-const TopChartCard = (song, i) => (
+const TopChartCard = ({song, i, isPlaying, activeSong, handlePauseClick, handlePlayClick}) => {
+  // const { data } = useGetTopChartsQuery();
+  return(
   <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
-    {song.title}
+    <h3 className="font -bold text-base text-white mr-3">{i + 1}.</h3>
+    <div className="flex-1 flex flex-row justify-between items-center">
+        <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song.title} />
+        <div className="flex-1 flex flex-col justify-center mx-3">
+            <Link to={`/song/${song.key}`}>
+              <p className="text-xl font-bold text-white">{song.title}</p>
+            </Link>
+            <Link to={`/artists/${song?.artists[0].adamid}`}>
+              <p className="text-base text-gray-300 mt-1">{song.subtitle}</p>
+            </Link>
+        </div>
+    </div>
+    <PlayPause
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      song={song}
+      handlePause={handlePauseClick}
+      handlePlay={() => handlePlayClick(song, i)}
+    />
   </div>
-)
+)}
 
 
 const TopPlay = () => {
@@ -23,6 +43,7 @@ const TopPlay = () => {
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data } = useGetTopChartsQuery();
   const divRef = useRef(null);
+  
 
   useEffect(() => {
     divRef.current.scrollIntoView({ behavior: 'smooth' });
@@ -33,7 +54,7 @@ const TopPlay = () => {
   const handlePauseClick = () => {
     dispatch(playPause(false));
   };
-  const handlePlayClick = () => {
+  const handlePlayClick = (song, i) => {
     dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true));
   };
@@ -53,6 +74,10 @@ const TopPlay = () => {
               key={song.key}
               song={song}
               i={i}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={handlePlayClick}
             />
           ))}
         </div>
